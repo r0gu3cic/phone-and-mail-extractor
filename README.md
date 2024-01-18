f5e76658abff903050134e48c6eb705efcb8a860
fws-stefan <stefan.stosic@forwardslashny.com>
2024-01-18 15:57:45 +0100

    made the code more pythonic

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # phone-and-mail-extractor
+
 A simple script that checks the text for phone numbers and email addresses in it.
 
+## Usage
+
+Run the script using command `./phone_and_email_extractor.py` or `python3 phone_and_email_extractor.py` and follow the instructions
--- phone_and_mail_extractor.py
+++ phone_and_mail_extractor.py
@@ -1,19 +1,21 @@
-#phone number and email address extractor
+#!/usr/bin/env python3
+
+# Phone number and email address extractor
 import re
 text=input('Enter text with phone numbers and email addresses you want to extract:\n')
-#phone number regex
+# Phone number regex
 number=re.compile(r'''
-\d{10} #one of the ways to write a number in Serbia
+\d{10} # One of the ways to write a number in Serbia
 |
-\d{3}\-\d{3}\-\d{4} #another way
+\d{3}\-\d{3}\-\d{4} # Another way
 ''',re.VERBOSE)
 phone_numbers=number.findall(text)
-#email regex
+# Email regex
 mail=re.compile(r'''(
-[a-zA-Z0-9._%+-]+ # username
+[a-zA-Z0-9._%+-]+ # Username
 @ # @ symbol
-[a-zA-Z0-9.-]+ # domain name
-\.[a-zA-Z]{2,4} # dot-something
+[a-zA-Z0-9.-]+ # Domain name
+\.[a-zA-Z]{2,4} # Dot-something
 )''', re.VERBOSE)
 emails=mail.findall(text)
 if len(phone_numbers)==0:
