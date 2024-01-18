9aaf1ecc4c97c1a69290aa361e483b8999a5a903
fws-stefan <stefan.stosic@forwardslashny.com>
2024-01-18 16:01:24 +0100

    made the code more pythonic

--- phone_and_mail_extractor.py
+++ phone_and_mail_extractor.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
 
-# Phone number and email address extractor
 import re
-text=input('Enter text with phone numbers and email addresses you want to extract:\n')
-# Phone number regex
-number=re.compile(r'''
-\d{10} # One of the ways to write a number in Serbia
-|
-\d{3}\-\d{3}\-\d{4} # Another way
-''',re.VERBOSE)
-phone_numbers=number.findall(text)
-# Email regex
-mail=re.compile(r'''(
-[a-zA-Z0-9._%+-]+ # Username
-@ # @ symbol
-[a-zA-Z0-9.-]+ # Domain name
-\.[a-zA-Z]{2,4} # Dot-something
-)''', re.VERBOSE)
-emails=mail.findall(text)
-if len(phone_numbers)==0:
-    print('There are no phone numbers in the text')
-else:
-    print('Phone numbers in the text:')
-for i in range(len(phone_numbers)):
-    print(phone_numbers[i])
-print()
-if len(emails)==0:
-    print('There are no e-mail addresses in the text')
-else:
-    print('E-mail addresses in the text:')
-for i in range(len(emails)):
-    print(emails[i])
 
+def extract_phone_numbers(text):
+    # Phone number regex
+    number_pattern = re.compile(r'''
+        \d{10}              # One of the ways to write a number in Serbia
+        |
+        \d{3}-\d{3}-\d{4}   # Another way
+    ''', re.VERBOSE)
+    return number_pattern.findall(text)
 
+def extract_emails(text):
+    # Email regex
+    email_pattern = re.compile(r'''
+        ([a-zA-Z0-9._%+-]+   # Username
+        @                    # @ symbol
+        [a-zA-Z0-9.-]+       # Domain name
+        \.[a-zA-Z]{2,4})      # Dot-something
+    ''', re.VERBOSE)
+    return email_pattern.findall(text)
 
+def main():
+    text = input('Enter text with phone numbers and email addresses you want to extract:\n')
 
+    phone_numbers = extract_phone_numbers(text)
+    emails = extract_emails(text)
 
+    if not phone_numbers:
+        print('There are no phone numbers in the text')
+    else:
+        print('Phone numbers in the text:')
+        for number in phone_numbers:
+            print(number)
+    print()
+
+    if not emails:
+        print('There are no e-mail addresses in the text')
+    else:
+        print('E-mail addresses in the text:')
+        for email in emails:
+            print(email)
+
+if __name__ == "__main__":
+    main()
